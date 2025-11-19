b978808c7e71c186c3bd0a0f72f49b919f89be7d
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-11-19 15:13:50 +0530

    corrected api retry issue

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import api from './api';
 
 export const Login = async (payload) => {
-  const response = await api.post('/auth/login', payload, { requiresAuth: false });
+  const response = await api.post('/auth/login', payload, { requiresAuth: false, withCredentials: true });
   return response.data;
 };
 
